5a61d15fecde038f9347cdee3fed22dea31e3256
Paul Klemm <paul.klemm@sf.mpg.de>
2017-05-22 16:02:04 +0200

    Remove static constructTable function

--- src/components/Table.js
+++ src/components/Table.js
@@ -53,32 +53,6 @@ class Table extends React.Component{
 		);
 	}
 
-	constructTable() {
-		// Getting the dimensions like this is still a little hacky
-		let dimensions = Object.keys(this.props.data[1]);
-
-		let table = [];
-		// Add header table
-		let header = [];
-		for (let i in dimensions) {
-			const dimension = dimensions[i];
-			header.push(<th>{dimension}</th>);
-		}
-		// Push header to the table
-		table.push(<tr>{header}</tr>);
-		// Push rows
-		// for (let i in this.props.data) {
-		for (let i = 0; i < 5000; i++) {
-			let row = [];
-			for (let j in dimensions) {
-				const dimension = dimensions[j];
-				row.push(<th key={`row_${i}${j}`}>{`${i}+${this.props.data[i][dimension]}`}</th>);
-			}
-			table.push(<tr>{row}</tr>);
-		}
-		return table;
-	}
-
 	renderRequired(newRowTop, newRowBottom) {
 		// Prevent overflowing list
 		if (this.state.rowBottom >= this.props.data.length - 1 && newRowBottom >= this.props.data.length - 1)
