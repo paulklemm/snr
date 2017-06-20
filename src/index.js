22a61ece4a5ccb586aea40a9583f3968bde50dac
Paul Klemm <paul.klemm@sf.mpg.de>
2017-06-20 10:13:53 +0200

    Use webpack module.hot for hot reloading

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 
+const rootEl = document.getElementById('root');
+
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  rootEl
 );
+
+// https://medium.com/superhighfives/hot-reloading-create-react-app-73297a00dcad
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    )
+  })
+}
