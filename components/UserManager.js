13c01f9222e09803274456687c4d1a6f73dd6b8b
Paul Klemm <paul.klemm@sf.mpg.de>
2017-06-30 11:35:21 +0200

    Mover user management code into UserManager Object

--- components/UserManager.js
+++ components/UserManager.js
@@ -1,11 +1,50 @@
+// Require bcrypt for authentication
+const bcrypt = require('bcryptjs');
+const { timeStampLog } = require('./Helper')
+
 class UserManager {
   constructor(userPath) {
     this.userPath = userPath;
   }
+  /**
+   * Check if password and hash are matching
+   * @param {String} password to check
+   * @param {String} hash to check
+   * @return {Bool} result of check
+   */
+  checkPassword(password, hash) {
+    return (bcrypt.compareSync(password, hash));
+  }
 
-  makeNoise() {
-    console.log('User Manager on duty.');
+  /**
+   * Get the password hash for specified user
+   * @param {String} user to retreive the hash for
+   * @return Password hash or empty string if password is not defined
+   */
+  getPasswordHash(user) {
+    const userSettings = this.getUserSettings(user);
+    if (typeof userSettings === "undefined" || typeof userSettings.passwd === "undefined") {
+      timeStampLog(`Password or user not defined for ${user}`, true);
+      return ('');
+    } else
+      return (userSettings.passwd);
+  }
+
+  /**
+   * Get users settings object from users name
+   * @param {String} user name
+   * @return {Object} users settings file
+   */
+  getUserSettings(user) {
+    // Read the user settings
+    const userSettings = readJSONFSSync(`${this.userPath}${user}`);
+    // If settings are empty, show error
+    if (typeof userSettings === "undefined")
+      timeStampLog(`Cannot find user ${user}. Either user folder (${this.userPath}) is wrong or user does not exist.`, true);
+    // Return user Settings if everything works properly
+    return (userSettings);
   }
 };
 
+// Export UserManager object
 exports.UserManager = UserManager;
--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 // Conjunction with create-react-app from [https://github.com/fullstackreact/food-lookup-demo](https://github.com/fullstackreact/food-lookup-demo)
 // Node classes: https://stackoverflow.com/questions/42684177/node-js-es6-classes-with-require
 const express = require("express");
-// Require bcrypt for authentication
-const bcrypt = require('bcryptjs');
 // const UserManager = require("./UserManager");
-var { timeStampLog, readJSONFSSync } = require('./Components/Helper')
-var { UserManager } = require('./Components/UserManager');
-
-const userManager = new UserManager();
-userManager.makeNoise();
+const { timeStampLog, readJSONFSSync } = require('./Components/Helper')
+const { UserManager } = require('./Components/UserManager');
 
 /**
  * Read settings from file. Settings should contain:
@@ -25,50 +20,12 @@ function getSettings(path="server_settings.json") {
   return(settings);
 }
 
-/**
-   * Check if password and hash are matching
-   * @param {String} password to check
-   * @param {String} hash to check
-   * @return {Bool} result of check
-   */
-function checkPassword(password, hash) {
-  return (bcrypt.compareSync(password, hash));
-}
-
-/**
- * Get the password hash for specified user
- * @param {String} user to retreive the hash for
- * @return Password hash or empty string if password is not defined
- */
-function getPasswordHash(user, userFolderPath) {
-  const userSettings = getUserSettings(user, userFolderPath);
-  if (typeof userSettings === "undefined" || typeof userSettings.passwd === "undefined") {
-    timeStampLog(`Password or user not defined for ${user}`, true);
-    return('');
-  } else 
-    return(userSettings.passwd);
-}
-
-/**
- * Get users settings object from users name
- * @param {String} user name
- * @param {String} userFolderPath path to users folder
- * @return {Object} users settings file
- */
-function getUserSettings(user, userFolderPath) {
-  // Read the user settings
-  const userSettings = readJSONFSSync(`${userFolderPath}${user}`);
-  // If settings are empty, show error
-  if (typeof userSettings === "undefined")
-    timeStampLog(`Cannot find user ${user}. Either user folder (${userFolderPath}) is wrong or user does not exist.`, true);
-  // Return user Settings if everything works properly
-  return(userSettings);
-}
-
 //////////// End of function declarations
 
 // Read in the settings
 const settings = getSettings();
+// Create new usersManager object and pass path to users
+const userManager = new UserManager(settings.users);
 
 const app = express();
 app.set("port", process.env.PORT || settings.port);
