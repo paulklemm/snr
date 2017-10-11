9a31c69963d7ef8bedb55471bf830381b9605e5f
Paul Klemm <paul.klemm@sf.mpg.de>
2017-10-11 17:55:13 +0200

    Improve AirBnB styleguide compliance

--- client/src/components/LayoutFactory.js
+++ client/src/components/LayoutFactory.js
@@ -14,38 +14,44 @@ class LayoutFactory {
     this.heights = {
       mainView: -1,
       smallMultiples: -1
-    }
+    };
   }
 
   updateWindowSize(windowWidth, windowHeight) {
     this.windowWidth = windowWidth;
     this.windowHeight = windowHeight;
-    this.updateHeights()
+    this.updateHeights();
   }
   setSmallMultiplesCount(smallMultiplesCount) {
     this.smallMultiplesCount = smallMultiplesCount;
-    this.updateHeights()
+    this.updateHeights();
   }
   increaseSmallMultiplesCount() {
-    this.smallMultiplesCount++;
-    this.updateHeights()
+    this.smallMultiplesCount += 1;
+    this.updateHeights();
   }
   decreaseSmallMultiplesCount() {
-    this.smallMultiplesCount--;
-    this.updateHeights()
+    this.smallMultiplesCount -= 1;
+    this.updateHeights();
   }
 
   updateHeights(debug = false) {
-    this.heights.mainView =         this.windowHeight - this.staticElementsHeights.header - this.staticElementsHeights.table - this.heigthPadding;
+    this.heights.mainView =
+      this.windowHeight -
+      this.staticElementsHeights.header -
+      this.staticElementsHeights.table -
+      this.heigthPadding;
     const numTileRows = Math.ceil(this.smallMultiplesCount / 2);
     // Weirdly enough the height must be adjusted by a constant factor
-    this.heights.smallMultiples = (this.heights.mainView + 10 - (this.spacingSize * numTileRows)) / numTileRows;
+    this.heights.smallMultiples =
+      // eslint-disable-next-line no-mixed-operators
+      (this.heights.mainView + 10 - (this.spacingSize * numTileRows)) / numTileRows;
     if (debug) {
-      console.log(`--------------------------------------`);
+      console.log('--------------------------------------');
       console.log(`smallMultiplesCount: ${this.smallMultiplesCount}`);
       console.log(`numTileRows: ${numTileRows}, spacingSize: ${this.spacingSize}`);
       console.log(`MainView height: ${this.heights.mainView}, SmallMultiplesHeight: ${this.heights.smallMultiples}`);
-      console.log("this.heights.smallMultiples = (this.heights.mainView - (this.spacingSize * numTileRows)) / numTileRows");
+      console.log('this.heights.smallMultiples = (this.heights.mainView - (this.spacingSize * numTileRows)) / numTileRows');
       console.log(`${this.heights.smallMultiples} = (${this.heights.mainView} - (${this.spacingSize} * ${numTileRows})) / ${numTileRows}`);
     }
   }
