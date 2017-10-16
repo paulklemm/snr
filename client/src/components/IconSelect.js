7caa36e608fc2f69c72f569466a5683b319dafa4
Paul Klemm <paul.klemm@sf.mpg.de>
2017-10-16 11:19:17 +0200

    Refactor IconSelect

--- client/src/components/IconSelect.js
+++ client/src/components/IconSelect.js
@@ -19,29 +19,17 @@ class IconSelect extends Component {
   constructor(props) {
     super(props);
     this.handleButtonClick = this.handleButtonClick.bind(this);
-    // Convert DatasetIcons Object to array
-    this.iconsArray = Object.values(DatasetIcons);
-    // Contains the index of the icon names
-    this.iconNameToArrayIndex = {};
-    // Array index to icon name, used in handleManuItemClick to
-    // get the name of the icon from the array index
-    this.arrayIndexToIconName = [];
-    for (let i = 0; i < this.iconsArray.length; i++) {
-      this.iconNameToArrayIndex[Object.keys(DatasetIcons)[i]] = i;
-      this.arrayIndexToIconName.push(Object.keys(DatasetIcons)[i]);
-    }
     // Get the icon associated with the data set
-    let iconName = props.getDatasetIcon(props.datasetName);
-    const iconID = this.iconNameToArrayIndex[iconName];
+    const iconName = props.getDatasetIcon(props.datasetName);
     this.state = {
       anchorEl: undefined,
       open: false,
-      selectedIndex: iconID
+      selectedIndex: iconName
     };
   }
 
-  handleMenuItemClick (event, iconName, index) {
-    this.setState({ selectedIndex: index, open: false });
+  handleMenuItemClick (event, iconName) {
+    this.setState({ selectedIndex: iconName, open: false });
     this.props.setDatasetIcon(this.props.datasetName, iconName);
   }
 
@@ -57,8 +45,13 @@ class IconSelect extends Component {
     const classes = this.props.classes;
     return (
       <div>
-        <IconButton className={classes.iconSelect} aria-owns="simple-menu" aria-haspopup="true" onClick={this.handleButtonClick}>
-          {this.iconsArray[this.state.selectedIndex]}
+        <IconButton 
+          className={classes.iconSelect}
+          aria-owns="simple-menu"
+          aria-haspopup="true"
+          onClick={this.handleButtonClick}
+        >
+          {DatasetIcons[this.state.selectedIndex]}
         </IconButton>
         <Menu
           id="lock-menu"
@@ -66,13 +59,13 @@ class IconSelect extends Component {
           open={this.state.open}
           onRequestClose={this.handleRequestClose}
         >
-          {this.iconsArray.map((option, index) =>
+          {Object.values(DatasetIcons).map((icon, index) =>
             <MenuItem
-              key={`${option}+${index}`}
-              selected={index === this.state.selectedIndex}
-              onClick={event => this.handleMenuItemClick(event, Object.keys(DatasetIcons)[index], index)}
+              key={`${Object.keys(DatasetIcons)[index]}+${index}`}
+              selected={Object.keys(DatasetIcons)[index] === this.state.selectedIndex}
+              onClick={event => this.handleMenuItemClick(event, Object.keys(DatasetIcons)[index])}
             >
-              {option}
+              {icon}
             </MenuItem>,
           )}
         </Menu>
